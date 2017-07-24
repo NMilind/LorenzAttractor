4c491baa8c446e914afdd58e6e846e95b78005e2
NMilind <nikhil.milind@hotmail.com>
2017-07-24 18:27:27 -0400

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/LorenzAttractor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LorenzAttractor.dir/main.cpp.o"
+  "LorenzAttractor.pdb"
+  "LorenzAttractor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LorenzAttractor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
